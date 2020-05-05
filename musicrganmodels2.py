1759888498d1a89082b67bce1fa522e822b818a7
Robert Norwood <scoutrpn@gmail.com>
2020-05-04 19:49:16 -0500

    try to fix read model

--- musicrganmodels2.py
+++ musicrganmodels2.py
@@ -52,45 +52,46 @@ def train_sequence_generator(lookback = 25, bs = 200):
     data_array = np.zeros((bs,lookback))
     label_array = np.zeros((bs, 1))
     counter = 0
-    for subdir, dirs, files in os.walk(data_location):
-          for file in files:
-              #print os.path.join(subdir, file)
-              filepath = subdir + os.sep + file
-              # Decodes audio
-              if filepath.endswith(".mp3"):
-                    mp3_audio = AudioSegment.from_file(filepath, format="mp3")
-                    # rudimentary downsample factor of 3
-                    audio_array = mp3_audio.get_array_of_samples()[::4]
-                    audio_array = np.array(audio_array)
-                    audio_array = audio_array.astype('float32')
-                    l = len(audio_array)
-                    audio_array = audio_array.reshape((l,1))
-                    scaler = MinMaxScaler(feature_range=(-1,1))
-                    scaler.fit(audio_array)
-                    audio_array = scaler.transform(audio_array)
-                    audio_array = audio_array.reshape((1,l))
-                    audio_array = audio_array[0]
-                    audio_array = np.nan_to_num(audio_array, nan=0.0)
-                    if not np.isnan(audio_array).any():
-                        if not np.isinf(audio_array).any() :
-#                         https://kite.com/python/answers/how-to-check-for-nan-elements-in-a-numpy-array-in-python
-                            for i in range (0,len(audio_array) - lookback - 1,100):
-                                data = audio_array[i:i+lookback]
-        #                         data = data.reshape((1,lookback,1))
-                                label = audio_array[i+lookback+1:i+lookback+2]
-                                label.reshape((1,1))
-                                data_array[counter] = data
-                                label_array[counter] = label
-                                counter +=1
-                                if(counter == bs):
-                                    counter = 0
-                                    out_data = data_array.reshape(bs,lookback,1)
-                                    out_labels = label_array.reshape(bs,1)
-                                    yield (out_data,out_labels)
+    while True:
+        for subdir, dirs, files in os.walk(data_location):
+              for file in files:
+                  #print os.path.join(subdir, file)
+                  filepath = subdir + os.sep + file
+                  # Decodes audio
+                  if filepath.endswith(".mp3"):
+                        mp3_audio = AudioSegment.from_file(filepath, format="mp3")
+                        # rudimentary downsample factor of 3
+                        audio_array = mp3_audio.get_array_of_samples()[::4]
+                        audio_array = np.array(audio_array)
+                        audio_array = audio_array.astype('float32')
+                        l = len(audio_array)
+                        audio_array = audio_array.reshape((l,1))
+                        scaler = MinMaxScaler(feature_range=(-1,1))
+                        scaler.fit(audio_array)
+                        audio_array = scaler.transform(audio_array)
+                        audio_array = audio_array.reshape((1,l))
+                        audio_array = audio_array[0]
+                        audio_array = np.nan_to_num(audio_array, nan=0.0)
+                        if not np.isnan(audio_array).any():
+                            if not np.isinf(audio_array).any() :
+    #                         https://kite.com/python/answers/how-to-check-for-nan-elements-in-a-numpy-array-in-python
+                                for i in range (0,len(audio_array) - lookback - 1,100):
+                                    data = audio_array[i:i+lookback]
+            #                         data = data.reshape((1,lookback,1))
+                                    label = audio_array[i+lookback+1:i+lookback+2]
+                                    label.reshape((1,1))
+                                    data_array[counter] = data
+                                    label_array[counter] = label
+                                    counter +=1
+                                    if(counter == bs):
+                                        counter = 0
+                                        out_data = data_array.reshape(bs,lookback,1)
+                                        out_labels = label_array.reshape(bs,1)
+                                        yield (out_data,out_labels)
+                            else:
+                                print("inf found!")
                         else:
-                            print("inf found!")
-                    else:
-                        print("nan found!")
+                            print("nan found!")
 
 
                         
@@ -99,46 +100,47 @@ def test_sequence_generator(lookback = 25, bs = 200):
     data_array = np.zeros((bs,lookback))
     label_array = np.zeros((bs, 1))
     counter = 0
-    for subdir, dirs, files in os.walk(data_location):
-          for file in files:
-              #print os.path.join(subdir, file)
-              filepath = subdir + os.sep + file
-              # Decodes audio
-              if filepath.endswith(".mp3"):
-                    mp3_audio = AudioSegment.from_file(filepath, format="mp3")
-                    # rudimentary downsample factor of 3
-                    audio_array = mp3_audio.get_array_of_samples()[::4]
-                    audio_array = np.array(audio_array)
-                    audio_array = audio_array.astype('float32')
-                    l = len(audio_array)
-                    audio_array = audio_array.reshape((l,1))
-                    # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html
-                    scaler = MinMaxScaler(feature_range=(-1,1))
-                    scaler.fit(audio_array)
-                    audio_array = scaler.transform(audio_array)
-                    audio_array = audio_array.reshape((1,l))
-                    audio_array = audio_array[0]
-                    audio_array = np.nan_to_num(audio_array, nan=0.0)
-                    if not np.isnan(audio_array).any():
-                        if not np.isinf(audio_array).any() :
-#                         https://kite.com/python/answers/how-to-check-for-nan-elements-in-a-numpy-array-in-python
-                            for i in range (0,len(audio_array) - lookback - 1,100):
-                                data = audio_array[i:i+lookback]
-        #                         data = data.reshape((1,lookback,1))
-                                label = audio_array[i+lookback+1:i+lookback+2]
-                                label.reshape((1,1))
-                                data_array[counter] = data
-                                label_array[counter] = label
-                                counter +=1
-                                if(counter == bs):
-                                    counter = 0
-                                    out_data = data_array.reshape(bs,lookback,1)
-                                    out_labels = label_array.reshape(bs,1)
-                                    yield (out_data,out_labels)
+    while True:
+        for subdir, dirs, files in os.walk(data_location):
+              for file in files:
+                  #print os.path.join(subdir, file)
+                  filepath = subdir + os.sep + file
+                  # Decodes audio
+                  if filepath.endswith(".mp3"):
+                        mp3_audio = AudioSegment.from_file(filepath, format="mp3")
+                        # rudimentary downsample factor of 3
+                        audio_array = mp3_audio.get_array_of_samples()[::4]
+                        audio_array = np.array(audio_array)
+                        audio_array = audio_array.astype('float32')
+                        l = len(audio_array)
+                        audio_array = audio_array.reshape((l,1))
+                        # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.MinMaxScaler.html
+                        scaler = MinMaxScaler(feature_range=(-1,1))
+                        scaler.fit(audio_array)
+                        audio_array = scaler.transform(audio_array)
+                        audio_array = audio_array.reshape((1,l))
+                        audio_array = audio_array[0]
+                        audio_array = np.nan_to_num(audio_array, nan=0.0)
+                        if not np.isnan(audio_array).any():
+                            if not np.isinf(audio_array).any() :
+    #                         https://kite.com/python/answers/how-to-check-for-nan-elements-in-a-numpy-array-in-python
+                                for i in range (0,len(audio_array) - lookback - 1,100):
+                                    data = audio_array[i:i+lookback]
+            #                         data = data.reshape((1,lookback,1))
+                                    label = audio_array[i+lookback+1:i+lookback+2]
+                                    label.reshape((1,1))
+                                    data_array[counter] = data
+                                    label_array[counter] = label
+                                    counter +=1
+                                    if(counter == bs):
+                                        counter = 0
+                                        out_data = data_array.reshape(bs,lookback,1)
+                                        out_labels = label_array.reshape(bs,1)
+                                        yield (out_data,out_labels)
+                            else:
+                                print("inf found!")
                         else:
-                            print("inf found!")
-                    else:
-                        print("nan found!")
+                            print("nan found!")
 
                         
 def song_generator(lookback,model, starter,length = 20000):
@@ -155,7 +157,7 @@ def song_generator(lookback,model, starter,length = 20000):
     scaler = MinMaxScaler(feature_range=[-30000,30000])
     scaler.fit(newsong)
     newsong = scaler.transform(newsong)
-    newsong = newsong.reshape(l,l)
+    newsong = newsong.reshape(1,l)
     return newsong[lookback:]
     
 
